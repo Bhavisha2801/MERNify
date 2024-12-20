cca755fc50d734242e098f851d673b1b0220daf2
Bhavisha-Nayi <bhavisha.saarathi@outlook.com>
2024-12-20 20:58:49 +0530

    updated bacupdated register schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,9 +4,13 @@ const { mongoose } = require("mongoose");
 const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
     password: { type: String, required: true },
+    passwordConfirm: { type: String, required: true },
+    acceptTerms: { type: Boolean, required: true },
 });
 
 // Hash Password before saving
