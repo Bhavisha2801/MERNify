0b1d0e84103669da171e594261a2eae72d8896cd
Bhavisha-Nayi <bhavisha.saarathi@outlook.com>
2024-12-25 13:03:18 +0530

    update expire token time

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -25,7 +25,7 @@ const loginUser = async (req, res) => {
         const isMatch = await bcrypt.compare(password, user.password);
         if(!isMatch) return res.status(400).json({ message: 'Invalid Credentials' });
 
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {expiresIn: '1h'});
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {expiresIn: '1'});
         res.json({ token, user: { id: user.id, name: user.name} });
     } catch (error) {
         res.status(500).json({ message: error.message });
